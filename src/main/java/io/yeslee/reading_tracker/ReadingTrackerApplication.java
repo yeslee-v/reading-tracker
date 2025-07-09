6a428ce30c0c059d9d85bfbda80350351fdfb52a
Yeshin Lee <yeslee5004@gmail.com>
2025-07-09 22:13:42 +0900

    init: spring project

---
+++ src/main/java/io/yeslee/reading_tracker/ReadingTrackerApplication.java
@@ -0,0 +1,13 @@
+package io.yeslee.reading_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yeslee/reading_tracker/ReadingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yeslee.reading_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
