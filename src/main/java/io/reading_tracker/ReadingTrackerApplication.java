118ed6de6f73e5ea14ca67f0167e0cfdb167b4b0
Yeshin Lee <yeslee5004@gmail.com>
2025-07-13 15:47:36 +0900

    modify: package structure

--- src/main/java/io/reading_tracker/ReadingTrackerApplication.java
+++ src/main/java/io/reading_tracker/ReadingTrackerApplication.java
@@ -1,4 +1,4 @@
-package io.yeslee.reading_tracker;
+package io.reading_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/reading_tracker/ReadingTrackerApplicationTests.java
+++ src/test/java/io/reading_tracker/ReadingTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.yeslee.reading_tracker;
+package io.reading_tracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
