b5964582e7dc60f43ec47cc6800a2df7b613c9c1
Yeshin Lee <yeslee5004@gmail.com>
2025-10-06 20:06:23 +0900

    feat: Base Entity 생성

--- src/main/java/io/reading_tracker/ReadingTrackerApplication.java
+++ src/main/java/io/reading_tracker/ReadingTrackerApplication.java
@@ -2,7 +2,9 @@ package io.reading_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ReadingTrackerApplication {
 
