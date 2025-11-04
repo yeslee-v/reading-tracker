01837c30da5b31c55a17578a33f7d7ee83459a37
Yeshin Lee <yeslee5004@gmail.com>
2025-11-04 10:50:19 +0900

    test: 도서 추가, 수정 테스트 케이스 설계

--- src/test/java/io/reading_tracker/service/BookServiceTest.java
+++ src/test/java/io/reading_tracker/service/BookServiceTest.java
@@ -77,4 +77,102 @@ class BookServiceTest {
         .containsExactly(secondReading.getId(), firstReading.getId())
         .doesNotContain(latestReading.getId());
   }
+
+  @Test
+  @DisplayName("읽고 있는 책이 없다면 빈 배열을 반환한다")
+  void getBookList_returnsEmptyList() {
+    // given 로그인을 했는데
+    User user = userRepository.save(new User("tester", "tester@example.com", "local", "local-1"));
+
+    Book first = bookRepository.save(new Book("책 A", "저자 A", "출판사 A", "isbn-1"));
+    Book second = bookRepository.save(new Book("책 B", "저자 B", "출판사 B", "isbn-2"));
+    Book third = bookRepository.save(new Book("책 C", "저자 C", "출판사 C", "isbn-3"));
+
+    UserBook firstReading =
+        userBookRepository.save(new UserBook(user, first, State.PLANNED, 300, 120));
+    UserBook secondReading =
+        userBookRepository.save(new UserBook(user, second, State.COMPLETED, 320, 160));
+    UserBook latestReading =
+        userBookRepository.save(new UserBook(user, third, State.COMPLETED, 280, 280));
+
+    LocalDateTime base = LocalDateTime.now();
+    ReflectionTestUtils.setField(firstReading, "createdAt", base.minusMinutes(10));
+    ReflectionTestUtils.setField(secondReading, "createdAt", base.minusMinutes(5));
+    ReflectionTestUtils.setField(latestReading, "createdAt", base);
+
+    entityManager.flush();
+    entityManager.clear();
+
+    // when IN_PROGRESS 상태의 책이 없다면
+    int page = 0;
+    GetBookListResponse response1 = bookService.getBookList(user.getId(), State.IN_PROGRESS, page);
+
+    // then 빈 배열을 반환한다
+    assertThat(response1.books()).hasSize(0);
+  }
+
+  /** 도서 검색에 외부 API를 사용하므로 Controller에서 테스트 진행 */
+  //  @Test
+  //  @DisplayName("키워드로 검색하면 관련 도서 정보가 반환된다")
+  //  void searchBook_returnsMatchingBookList_ByKeyword() {
+  //    // given 키워드(도서명, ISBN 등)이 주어지면
+  //    // when 검색했을 때
+  //    // then 관련 도서 목록이 반환된다
+  //  }
+
+  @Test
+  @DisplayName("선택한 도서를 추가하면 IN_PROGRESS 상태인 새 도서 정보를 반환한다")
+  void addBook_returnsNewBookInformation() {
+    // given 선택한 도서를
+    // when 추가하면
+    // then IN_PROGRESS 상태인 새 도서 정보를 반환한다
+  }
+
+  @Test
+  @DisplayName("이미 추가된 도서는 내 도서 목록에 재추가할 수 없다")
+  void addAlreadyExistBook_throwsError() {
+    // given 이미 도서 목록(UserBookByUserId)에 있는 책을
+    // when 재추가하려고 하면
+    // then 이미 존재하는 책은 재추가할 수 없다고 에러를 반환한다
+  }
+
+  @Test
+  @DisplayName("읽은 페이지를 업데이트하면 독서 진행률이 수정된다")
+  void updateBook_withCurrentPage_returnsRateCalculated() {
+    // given 읽은 페이지를
+    // when 업데이트하면
+    // then 자동으로 독서 진행률이 수정된다
+  }
+
+  @Test
+  @DisplayName("읽은 페이지가 전체 페이지 값보다 크다면 에러를 반환한다")
+  void updateBook_withCurrentPageGreaterThanTotalPages_throwsError() {
+    // given 전체 페이지보다 값이 큰 읽은 페이지 값을
+    // when 업데이트하면
+    // then 읽은 페이지는 전체 페이지 값보다 작거나 같아야한다는 에러를 반환한다
+  }
+
+  @Test
+  @DisplayName("읽은 페이지가 음수이면 에러를 반환한다")
+  void updateBook_withNegativeCurrentPage_throwsError() {
+    // given 음수인 읽은 페이지 값을
+    // when 업데이트하면
+    // then 읽은 페이지는 전체 페이지 값보다 작거나 같아야한다는 에러를 반환한다
+  }
+
+  @Test
+  @DisplayName("읽은 페이지 값이 전체 페이지 값과 같다면 상태는 COMPLETE로 바뀐다")
+  void updateBook_withCurrentPageSameAsTotalPages_returnsStateCompleteUpdated() {
+    // given 읽은 페이지 값이
+    // when 전체 페이지 값과 같다면
+    // then 상태가 COMPLETED로 자동으로 바뀐다
+  }
+
+  @Test
+  @DisplayName("COMPLETE 상태에서 읽은 페이지 값을 수정하면 상태는 IN_PROGRESS로 바뀐다")
+  void updateBook_withCurrentPageModifiedNotSameAsTotalPages_returnsStateInProgressUpdated() {
+    // given COMPLETE 상태에서 읽은 페이지 값
+    // when 전체 페이지 값과 다르게 수정하면
+    // then 상태가 IN_PROGRESS로 바뀐다
+  }
 }
