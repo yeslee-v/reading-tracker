f2e7c684473f4ec4b9447aeea34fe09327d3bb8c
Yeshin Lee <yeslee5004@gmail.com>
2025-11-08 17:12:01 +0900

    refactor: provider/ID 조회 로직 AuthRepository로 이동

--- src/main/java/io/reading_tracker/repository/AuthRepository.java
+++ src/main/java/io/reading_tracker/repository/AuthRepository.java
@@ -1,12 +1,11 @@
 package io.reading_tracker.repository;
 
-import io.reading_tracker.domain.user.User;
+import io.reading_tracker.domain.user.Auth;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-  Optional<User> findByEmail(String email);
+public interface AuthRepository extends JpaRepository<Auth, Long> {
+  Optional<Auth> findByProviderAndProviderId(String provider, String providerId);
 }
