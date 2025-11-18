a890dd8b92d0c38d75a8797c683032e09734b667
Yeshin Lee <yeslee5004@gmail.com>
2025-11-18 10:05:12 +0900

    feat: 캐싱 활성화

--- src/main/java/io/reading_tracker/ReadingTrackerApplication.java
+++ src/main/java/io/reading_tracker/ReadingTrackerApplication.java
@@ -2,12 +2,13 @@ package io.reading_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class ReadingTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReadingTrackerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ReadingTrackerApplication.class, args);
+  }
 }
