7bf128ca339401772299422c02766a626680c4ec
Yeshin Lee <yeslee5004@gmail.com>
2025-11-20 20:03:06 +0900

    fix: naver refresh token 길이로 인코딩 방법 PBKDF2로 수정

--- src/main/java/io/reading_tracker/config/SecurityConfig.java
+++ src/main/java/io/reading_tracker/config/SecurityConfig.java
@@ -12,8 +12,9 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -73,7 +74,8 @@ public class SecurityConfig {
 
   @Bean
   public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+    return new Pbkdf2PasswordEncoder(
+        "reading-tracker-secret", 16, 310000, SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256);
   }
 
   @Bean
