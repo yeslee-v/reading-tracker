c7bd058c593f8f3cfc4074f13bbf3f3c8b936fba
Yeshin Lee <yeslee5004@gmail.com>
2025-11-23 19:31:20 +0900

    test: 유저 서비스 테스트 4개 작성

--- src/main/java/io/reading_tracker/service/UserServiceImpl.java
+++ src/main/java/io/reading_tracker/service/UserServiceImpl.java
@@ -20,7 +20,9 @@ public class UserServiceImpl implements UserService {
   @Override
   public UserResponse getUserById(Long userId) {
     User user =
-        userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new UserNotFoundException("유효하지 않은 사용자입니다"));
 
     return UserResponse.from(user);
   }
@@ -29,7 +31,9 @@ public class UserServiceImpl implements UserService {
   @Transactional
   public UpdateNicknameResponse updateNickname(Long userId, UpdateNicknameRequest request) {
     User user =
-        userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException(userId));
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new UserNotFoundException("유효하지 않은 사용자입니다"));
     user.updateNickname(request.nickname());
 
     return UpdateNicknameResponse.from(user);
